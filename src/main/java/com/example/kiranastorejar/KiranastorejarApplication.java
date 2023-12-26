6e46711bd42d3881b28faa3d006e03f23f36f191
Mohit Khairnar <mvk1407@gmail.com>
2023-12-26 22:46:17 +0530

    feat: initialised springweb

---
+++ src/main/java/com/example/kiranastorejar/KiranastorejarApplication.java
@@ -0,0 +1,13 @@
+package com.example.kiranastorejar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiranastorejarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiranastorejarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kiranastorejar/KiranastorejarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kiranastorejar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiranastorejarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
